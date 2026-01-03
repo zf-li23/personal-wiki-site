511b533051c9e88ab30778e69e110099093e1589
zf-li23 <zf-li23@mails.tsinghua.edu.cn>
2026-01-03 21:21:42 +0800

    refactor: switch to BrowserRouter and add 404.html for GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Wiki from './pages/Wiki';
 
 function App() {
   return (
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <Router basename="/personal-wiki-site" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
